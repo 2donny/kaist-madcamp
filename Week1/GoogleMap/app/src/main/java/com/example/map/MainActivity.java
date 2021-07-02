c3d13655d33ad6de4968f38020d127d879c2087c
EdonJung <2donny@naver.com>
2021-07-02 14:54:03 +0900

    Week1: google map cluster api

--- Week1/GoogleMap/app/src/main/java/com/example/map/MainActivity.java
+++ Week1/GoogleMap/app/src/main/java/com/example/map/MainActivity.java
@@ -17,10 +17,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.clustering.ClusterManager;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-
+    private Random mRandom = new Random(1984);
     private GoogleMap googleMap;
+    private double lat = 36.374171;
+    private double lng = 127.365293;
+
+    private ClusterManager<MyItem> clusterManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +39,49 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         this.googleMap = googleMap;
-        LatLng latLng = new LatLng(36.374171, 127.365293);
+        LatLng latLng = new LatLng(lat, lng);
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.title("카이스트");
         markerOptions.snippet("N1");
         markerOptions.position(latLng);
 
         googleMap.addMarker(markerOptions);
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
 
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             googleMap.setMyLocationEnabled(true);
         } else {
             checkLocationPermissionWithRationale();
+        }
+
+        clusterManager = new ClusterManager<MyItem>(this, googleMap);
+        googleMap.setOnCameraIdleListener(clusterManager);
+        googleMap.setOnMarkerClickListener(clusterManager);
+
+        clusterManager.setOnClusterItemClickListener(new ClusterManager.OnClusterItemClickListener<MyItem>() {
+            @Override
+            public boolean onClusterItemClick(MyItem item) {
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.title(item.getTitle());
+                markerOptions.position(item.getPosition());
+                googleMap.animateCamera(CameraUpdateFactory.newLatLng(item.getPosition()));
+                Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        addItems();
+    }
+
+    private void addItems() {
+        String[] classMates = new String[]{"정이든", "강수아", "이혜민", "송예은", "양현호", "김경하", "금나연", "김영경", "김장현", "김주형", "김찬영", "김태우", "김현수", "박종서", "송인화", "최정재", "송재현", "양현호", "이호준", "정나연", "종회", "주은", "최종윤", "최지호", "추승우"};
+
+        for(int i = 0 ; i < 24; i++) {
+            MyItem offsetItem = new MyItem(position(), classMates[i]);
+            clusterManager.addItem(offsetItem);
         }
     }
 
@@ -70,6 +105,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+//        36.374171, 127.365293
+    private LatLng position() {
+        return new LatLng(random(36.374171, 36.375171), random(127.365293, 127.363393));
+    }
+
+    private double random(double min, double max) {
+        return mRandom.nextDouble() * (max - min) + min;
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
